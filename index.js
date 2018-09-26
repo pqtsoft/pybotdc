c437525b987825a6678c30bec8ba50aff6ff0b5c
Thang <pqthang97@gmail.com>
2018-09-26 11:38:29 +0700

    test avatar

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ client.on("message", async message => {
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
+  if(command === "avatar") {
+    let member = message.mentions.members.first() || message.author;
+    message.channel.send(member.avatarURL);
+  }
+
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
